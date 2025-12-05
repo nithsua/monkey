f4a151cc70bed9efd8c6c4d822e31ea00611fd51
nithsua <nivasmuthu452@gmail.com>
2025-12-06 01:27:02 +0530

    [INIT]: Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nithsua/monkey
+
+go 1.25.3
---
+++ token/token.go
@@ -0,0 +1,40 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	// Delimiters
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "fn"
+	LET      = "let"
+	TRUE     = "true"
+	FALSE    = "false"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
+)
