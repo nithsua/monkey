053274825b37f714f98f23fa7fff16a53f4fffd5
nithsua <nivasmuthu452@gmail.com>
2025-12-13 17:51:40 +0530

    [RFCT]: Base REPL

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/nithsua/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in command\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
